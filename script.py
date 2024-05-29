59b2e5bc96d101b0ba1a401bfb41cc0f2776520d
rhuaannn <rhuanmarques1223@gmail.com>
2024-05-29 09:00:43 -0300

    script- yt

---
+++ script.py
@@ -0,0 +1,14 @@
+from pytube import YouTube, streams
+from pytube.cli import on_progress
+
+link = input("Digite a URL")
+
+yt = YouTube(link, on_progress_callback=on_progress)
+
+print("Titulo", yt.title)
+
+print("Baixando..")
+
+ys = yt.streams.get_highest_resolution()
+
+ys.download()
